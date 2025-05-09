e05cbe62c68a0f265955748d34457885f492c9cd
din <dudinsdn@gmail.com>
2025-05-09 17:08:46 +0700

    setup and config user - main

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"log"
 	"my/go-api/config"
+	"my/go-api/internal/handler"
+	"my/go-api/internal/repository"
+	"my/go-api/internal/usecase"
 	"net/http"
 )
 
 func main() {
 	cfg := config.Load()
+	db := config.InitDB(cfg.DB)
+
+	userRepo := repository.NewUserRepository(db)
+	userUC := usecase.NewUserUsecase(userRepo)
+	userHandler := handler.NewUserHandler(userUC)
+
+	http.HandleFunc("/users", userHandler.HandleGetUsers)
 
 	log.Printf("Server running at %s", cfg.Server.Port)
 	log.Fatal(http.ListenAndServe(cfg.Server.Port, nil))
--- config/config.go
+++ config/config.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
 type DBConfig struct {
 	DSN string
 }
@@ -18,4 +24,12 @@ func Load() *Config {
 		DB:     DBConfig{DSN: "din:dbdin123@tcp(localhost:3306)/go_api"},
 		Server: ServerConfig{Port: ":8080"},
 	}
+}
+
+func InitDB(cfg DBConfig) *sql.DB {
+	db, err := sql.Open("mysql", cfg.DSN)
+	if err != nil {
+		panic(err)
+	}
+	return db
 }
