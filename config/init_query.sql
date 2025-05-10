285a0407df2ec778614533a18789360ee521bffc
din <dudinsdn@gmail.com>
2025-05-10 11:05:38 +0700

    init query mysql

---
+++ config/init_query.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE go_api; 
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
+);
+
+INSERT INTO users (name) VALUES ('Alice'), ('Bob'), ('Charlie');
